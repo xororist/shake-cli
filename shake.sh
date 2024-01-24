cb9546025c1d73dc0210311a42ebfe5cb3384121
xororist <hcachon@myges.fr>
2024-01-24 16:54:59 +0100

    Update shake.sh

--- shake.sh
+++ shake.sh
@@ -13,8 +13,8 @@ else
     exit 1
 fi
 
-# Check if pip3 is installed installed in order to install dependencies
-echo "[-] Checking if python3 is installed [-]"
+# Check if pip3 is installed in order to install dependencies
+echo "[-] Checking if pip3 is installed [-]"
 if command -v pip3 >/dev/null 2>&1; then
     echo "[OK] pip3 is installed!"
 else
@@ -24,12 +24,12 @@ else
     exit 1
 fi
 
-# Upgrade pip3 to latest version
-echo "[-] Upgrading pip3 to latest version [-]"
+# Upgrade pip3 to the latest version
+echo "[-] Upgrading pip3 to the latest version [-]"
 if pip3 install --upgrade pip; then
-    echo "[OK] pip3 has been upgraded to latest version."
+    echo "[OK] pip3 has been upgraded to the latest version."
 else
-    echo "[ERR] Please update pip3 to latest version in order to install required dependencies for Shake CLI..."
+    echo "[ERR] Please update pip3 to the latest version in order to install required dependencies for Shake CLI..."
     echo "[ERR] Exiting..."
     exit 1
 fi
@@ -38,6 +38,18 @@ fi
 echo "[-] Installing Shake CLI [-]"
 if python3 -m pip install .; then
     echo "[-] Shake CLI has been successfully installed! [-]"
+
+    shake_dir="$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["scripts"])')"
+    
+    # Add the shake command to PATH if it is not already in the ~/.bashrc file
+    if [[ ":$PATH:" != *":$shake_dir:"* ]]; then
+        echo "export PATH=\"\$PATH:$shake_dir\"" >> ~/.bashrc
+        echo "[-] Shake CLI directory has been successfully added to PATH! [-]" 
+        echo "[-] Please restart your terminal or run 'source ~/.bashrc' in order to run the Shake CLI. [-]"
+    else
+        echo "[-] Shake CLI is already registered in PATH [-]"
+    fi
+    
     echo "[-] Start using it by running the 'shake' command! [-]"
 else
     echo "[ERR] Shake CLI installation failed..."
